3e7b0bf2d7836a144111ad35b9a13bb5e861549a
mdugmore <michael.dugmore@gmail.com>
2014-04-04 15:40:50 +0100

    Initial Commit

---
+++ SupportWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SupportWebApplication.Startup))]
+namespace SupportWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
